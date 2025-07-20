8edd8313b469755f6f2e95414b2e3adf9cbaff0b
OLDecker <ole.decker.od@gmail.com>
2025-07-20 22:38:10 +0200

    Refactor training configuration and tokenizer setup; add protein vocabulary file

--- run_training.sh
+++ run_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --partition=gpu-single
-#SBATCH --time=00:10:00
+#SBATCH --time=11:00:00
 #SBATCH --mem=32G
 #SBATCH --gres=gpu:A100:1
 #SBATCH --job-name=xlstm_test
@@ -13,4 +13,6 @@ mamba activate xlstm
 echo $CONDA_DEFAULT_ENV
 
 # Run the training
+echo "run with learningrate 1.6e-4"
 $CONDA_PREFIX/bin/python train.py configs/go_xlstm_classification.yaml
+
