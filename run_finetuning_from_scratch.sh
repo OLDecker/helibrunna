337cfeb821f1f064be596ddff622a686a967c8ca
OLDecker <ole.decker.od@gmail.com>
2025-07-23 19:03:10 +0200

    correcting sbatch

--- run_finetuning_from_scratch.sh
+++ run_finetuning_from_scratch.sh
@@ -2,14 +2,15 @@
 #SBATCH --job-name=xlstm_scratch
 #SBATCH --output=logs/xlstm_scratch_%j.out
 #SBATCH --error=logs/xlstm_scratch_%j.err
-#SBATCH --time=24:00:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+##SBATCH --gres=gpu:A40:1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
+#SBATCH --partition=cpu-single
 
 # Activate Conda environment
-source /etc/profile.d/conda.sh
+module load devel/miniforge
 mamba activate xlstm
 
 # Run the training from scratch script
